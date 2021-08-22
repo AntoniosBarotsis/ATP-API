70e2e98a29dabffcc7b088bc658c42609fb7711d
AntoniosBarotsis <a.barotsis@student.tudelft.nl>
2021-08-22 18:11:01 +0200

    Implemented basic cach interaction

--- R/app.R
+++ R/app.R
@@ -1,4 +1,5 @@
 library(plumber)
+library(redux)
 
 port <- Sys.getenv('PORT')
 
@@ -10,6 +11,12 @@ server <- plumb("plumber.R")
 
 players <- pr("players.R")
 server %>% pr_mount("/players", players)
+
+# Set r to NULL if no redis database is detected
+r <- tryCatch(
+  { redux::hiredis() }, 
+  error = function(e) NULL
+)
 
 server$run(
 	host = '0.0.0.0',
--- R/players.R
+++ R/players.R
@@ -3,5 +3,29 @@
 #* @param name The name of the player
 #* @get /<name>
 function(name) {
-  list(msg = paste0("Hello ", name))
+  res <- getAndSetPlayer(name)
+  
+  if (is.null(res)) {
+    list(msg = paste0("Hello ", "(scraped data)", " [FROM SITE]"))
+  } else {
+    list(msg = paste0("Hello ", res, " [FROM CACHE]"))
+  }
+}
+
+getAndSetPlayer <- function(name) {
+  # No redis db detected
+  if (is.null(r)) {
+    return(NULL)
+  }
+
+  res <- r$GET(paste0("player::", name))
+  
+  # Cache Miss
+  if (is.null(res)) {
+    r$command(c("SET", paste0("player::", name), "foo", "EX", 60))
+    
+    return(NULL)
+  } 
+
+  return(res)
 }
